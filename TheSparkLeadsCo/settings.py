2eefcda72ab87a9df4b27d8f1fa03db61aa8f2cb
Irasubiza-Tharcisse <irasubizatharcisse23@tuta.io>
2026-03-12 22:43:37 +0200

    first commit

--- TheSparkLeadsCo/settings.py
+++ TheSparkLeadsCo/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
+import os
 
 import os
 from pathlib import Path
@@ -118,10 +119,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-    ]
+    BASE_DIR / "static", 
+]
