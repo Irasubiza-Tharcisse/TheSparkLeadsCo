f8a692fbad68696f500ae403011e6ac6b70061e8
Irasubiza Tharcisse <irasubizatharcisse23@tuta.io>
2026-03-12 22:39:22 +0200

    Configure static files settings in settings.py
    Updated STATIC_URL and added STATIC_ROOT configuration.

--- TheSparkLeadsCo/settings.py
+++ TheSparkLeadsCo/settings.py
@@ -117,9 +117,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+import os
 
-# This is the most common reason for the "Yellow 404" error
+STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    BASE_DIR / "static", 
-]
+    os.path.join(BASE_DIR, 'static')
+    ]
