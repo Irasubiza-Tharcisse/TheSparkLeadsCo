7be7cdf351542c75f7ef8cd36a59eb788d6ffbe7
Irasubiza Tharcisse <irasubizatharcisse23@tuta.io>
2026-03-15 10:26:16 +0200

    Clean up settings.py by removing conflict markers
    Removed merge conflict markers from settings.py

--- TheSparkLeadsCo/settings.py
+++ TheSparkLeadsCo/settings.py
@@ -117,10 +117,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-<<<<<<< HEAD
-=======
-
->>>>>>> ed758b8 (first commit)
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
